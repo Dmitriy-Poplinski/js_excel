a9a09dbd7105cca2a75f1effc8c1f9e128c684ac
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-08-19 23:46:15 +0300

    added listeners structure

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,9 +1,16 @@
 import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.listeners);
+    };
 
     // return componet`s template
     toHTML () {
         return '';
     };
+
+    init() {
+        this.initDomListeners();
+    }
 };
