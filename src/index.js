7bffb718aeef6c9a4825b67341cd719b65d6ce89
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-08-19 18:37:02 +0300

    Partly realized Excel component, created Formula, Header, Table and Toolbar components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { Excel } from '@/components/excel/Excel';
+import { Header } from '@/components/header/header';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Table } from '@/components/table/Table';
+
 import './scss/index.scss';
 
+
 const excel = new Excel('#app', {
-    components: []
+    components: [Header, Toolbar, Formula, Table]
 });
+
+excel.render();
