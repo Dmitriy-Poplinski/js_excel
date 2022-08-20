11f5e9423adb4bbe3c8eb9fc8f9aa7ecd144ba12
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-08-20 23:31:38 +0300

    realized multi selection on shift key

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,9 @@
 import { ExcelComponent } from '@core/ExcelComponent';
-import {$} from '@core/dom';
+import { $ } from '@core/dom';
 
 import { createTable } from './table.template';
 import { resizeHandler } from './table.resize';
-import { isCell, shouldResize } from './table.functions';
+import { isCell, shouldResize, matrix } from './table.functions';
 import { TableSelection } from './TableSelection';
 
 export class Table extends ExcelComponent {
@@ -35,7 +35,13 @@ export class Table extends ExcelComponent {
             resizeHandler(this.$root, event);
         } else if (isCell(event)) {
             const $target = $(event.target);
-            this.selection.select($target);
+            if (event.shiftKey) {
+                const $cells = matrix($target, this.selection.current).map(id => this.$root.find(`[data-id="${id}"]`));
+                this.selection.selectGroup($cells);
+                
+            } else {
+                this.selection.select($target);
+            };
         };
     };
 };
