08db7b0837b35e3c5175fce0f04a6baf4c2dbcc7
dmytro-poplinski <dmytro.poplinski@gmail.com>
2022-08-21 18:25:05 +0300

    optimized subscribe logik

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -41,10 +41,6 @@ export class Table extends ExcelComponent {
         this.$on('formula:done', () => {
             this.selection.current.focus();
         });
-
-        // this.$subscribe(state => {
-        //     console.log('Table state', state);
-        // });
     };
 
     selectCell($cell) {
